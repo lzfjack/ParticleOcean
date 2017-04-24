ac3edc57f4ce8cb57181314def1208e7d7080f1b
li zifeng <476108172@qq.com>
2017-04-24 14:22:44 +0800

    basic sources code

---
+++ Assets/Script/Ocean.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ocean : MonoBehaviour {
+
+    public ParticleSystem pSystem;
+    private ParticleSystem.Particle[] particleArray;
+    public Gradient colorGradient;
+
+    public int seaResolution = 20;
+    public float spacing = 0.5f;
+
+    public float noiseScale = 0.2f;
+    public float heightScale = 3f;
+
+    private float perlinNoiseAnimX = 0.01f;
+    private float perlinNoiseAnimY = 0.01f;
+
+    // Use this for initialization
+    void Start () {
+        particleArray = new ParticleSystem.Particle[seaResolution * seaResolution];
+        pSystem.maxParticles = seaResolution * seaResolution;
+        pSystem.Emit(seaResolution * seaResolution);
+        pSystem.GetParticles(particleArray);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < seaResolution; i++) {
+            for (int j = 0; j < seaResolution; j++) {
+                float zPos = Mathf.PerlinNoise(i * noiseScale + perlinNoiseAnimX, j * noiseScale + perlinNoiseAnimY) * heightScale;
+                particleArray[i * seaResolution + j].color = colorGradient.Evaluate(zPos);
+                particleArray[i * seaResolution + j].position = new Vector3(i * spacing, zPos, j * spacing);
+            }
+        }
+
+        perlinNoiseAnimX += 0.01f;
+        perlinNoiseAnimY += 0.01f;
+
+        pSystem.SetParticles(particleArray, particleArray.Length);
+    }
+}
---
+++ Assets/Script/Ring.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour {
+
+    public ParticleSystem pSystem;
+    private ParticleSystem.Particle[] particleArray;
+    public Gradient colorGradient;
+
+    public float ringResolution = 500;
+    public float minRadius = 4;
+    public float maxRadius = 10;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
